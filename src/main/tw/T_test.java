8fb30b6e8ad99d4c0d4b7586051814d611687d8e
xiaofei <xiaofei19921109@163.com>
2019-07-16 21:46:51 +0800

    inner person test

--- src/main/tw/T_test.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.test;
-
-import java.util.Arrays;
-
-public class T1 {
-    public static void main(String[] args) throws InterruptedException {
-        System.out.println("Hello world!");
-    }
-}
